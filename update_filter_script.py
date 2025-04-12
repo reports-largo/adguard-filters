d29efb738f0d89721976975541cb524c1a9466ab
reports-largo <reports.largo-9n@icloud.com>
2025-04-12 19:28:23 +0900

    Update update_filter_script.py

--- update_filter_script.py
+++ update_filter_script.py
@@ -2,7 +2,7 @@ import requests
 from datetime import datetime
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
     'Referer': 'https://280blocker.net/'
 }
 
